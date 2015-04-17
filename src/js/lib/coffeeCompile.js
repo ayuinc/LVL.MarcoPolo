17e0a11bf87ee0472b181df1b86deaac407cfa44
Gus <yo@soygus.com>
2015-04-17 12:28:12 -0500

    Init isotope

--- src/js/lib/coffeeCompile.js
+++ src/js/lib/coffeeCompile.js
@@ -1,15 +1,5 @@
 (function() {
-  $(document).ready(function() {
-    var $container, itemTempl;
-    $container = $('.pins-grid');
-    $container.imagesLoaded(function() {
-      $container.isotope({
-        itemSelector: '.grid-list-item',
-        layoutMode: 'fitRows'
-      });
-    });
-    itemTempl = '<div class="grid-list-item">' + '<div class="pin">' + '<div class="pin-top">' + '<small class="pin-category">{categories show_group="1"}{category_name}{/categories} in {categories show_group="3"}{category_name}{/categories}</small>' + '<h2 class="pin-title"><a href="{site_url}project/{url_title}">{title}</a></h2>' + '<em class="pin-location">{categories show_group="2"}{category_name}{/categories}</em>' + '</div>' + '<div class="pin-center">' + '<div class="pin-pic">' + '{project_imagenes limit="1"}' + '<img src="{image:url:small}" alt="Grid 1" class="ignore-srcset">' + '{/project_imagenes}' + '<a href="{site_url}project/{url_title}" class="overlay">' + '<div class="pin-bottom">' + '<button class="btn btn-icon-custom btn-gray-darker btn-icon-left btn-sm">' + '<i>45</i>' + '<span>VOTE</span>' + '</button>' + '</div>' + '</a>' + '</div>' + '</div>' + '</div>' + '</div>';
-  });
+
 
 }).call(this);
 
